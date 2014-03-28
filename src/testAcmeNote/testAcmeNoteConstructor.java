3121c400203851a828160268911e85cbc60c27c5
Shaun Christensen <shaunebear@live.com>
2014-03-27 20:34:28 -0600

    Created JUnit test package "testAcmeNote". Created initial test case to check the default constructor as a prototype.

---
+++ src/testAcmeNote/testAcmeNoteConstructor.java
@@ -0,0 +1,12 @@
+package testAcmeNote;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testAcmeNoteConstructor
+{
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
